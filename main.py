dcc4587067e29fd31c8eb3d7e77f3f777ea619af
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-19 19:51:39 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,7 @@
+def rotate_list(lst, k):
+    k = k % len(lst)
+    return lst[k:] + lst[:k]
+
+print(rotate_list([1,2,3,4,5], 2))  # [3, 4, 5, 1, 2]
+print(rotate_list([1,2,3,4,5], 5))  # [1, 2, 3, 4, 5]
+print(rotate_list([1,2,3,4,5], 7))  # [3, 4, 5, 1, 2]
